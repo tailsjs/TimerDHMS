991f60d969b8668a78bb3bfb632f6406f0ca0d4b
TweYorZ <49571184+TweYorZ@users.noreply.github.com>
2019-04-29 06:49:12 +0300

    Первая версия, ах)

---
+++ timer.js
@@ -0,0 +1,53 @@
+let seconds = 0;
+let minutes = 0;
+let hours = 0;
+let days = 0;
+let strSec;
+let strMin;
+let strHou;
+let strDay;
+setInterval(function() {
+	let testSeconds = seconds + 1
+	let testMinutes = minutes + 1
+	let testHours = hours + 1
+	let testDays = days + 1
+	if(testSeconds === 60){
+		seconds = 0;
+		minutes += 1
+	}
+	if(testMinutes === 60){
+		minutes = 0;
+		hours += 1
+	}
+	if(testHours === 24){
+		hours = 0;
+		days += 1
+	}
+	seconds += 1
+	if(seconds < 10){
+		strSec = `0${seconds}`
+	}
+	if(minutes < 10){
+		strMin = `0${minutes}`
+	}
+	if(hours < 10){
+		strHou = `0${hours}`
+	}
+	if(days < 10){
+		strDay = `0${days}`
+	}
+	if(days > 9){
+		strDay = `${days}`
+	}
+	if(hours > 9){
+		strHou = `${hours}`
+	}
+	if(minutes > 9){
+		strMin = `${minutes}`
+	}
+	if(seconds > 9){
+		strSec = `${seconds}`
+	}
+     console.log(`Время со старта скрипта:\n${strDay}:${strHou}:${strMin}:${strSec}`)
+	
+}, 1000);
